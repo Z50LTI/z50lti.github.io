03cb4ca78fbf2cf932009b0cdeb35a0f87884272
Z50LTI <92446963+Z50LTI@users.noreply.github.com>
2023-05-04 10:42:42 +0200

    Update darkmode.js

--- js/darkmode.js
+++ js/darkmode.js
@@ -5,13 +5,13 @@ modeBtn.onchange = (e) => {
         document.documentElement.classList.remove("light")
         document.documentElement.classList.add("dark")
         window.localStorage.setItem('mode', 'dark');
-        document.getElementById("bannerimage").src = ".../images/Welcome-to-the-Silver-Glass-Mobile-Bar(night mode).webp";
+        document.getElementById("bannerimage").src = "../images/Welcome-to-the-Silver-Glass-Mobile-Bar(night mode).webp";
 
     } else {
         document.documentElement.classList.remove("dark")
         document.documentElement.classList.add("light")
         window.localStorage.setItem('mode', 'light');
-        document.getElementById("bannerimage").src = ".../images/Welcome-to-the-Silver-Glass-Mobile-Bar.webp";
+        document.getElementById("bannerimage").src = "../images/Welcome-to-the-Silver-Glass-Mobile-Bar.webp";
     }
 }
 
@@ -20,13 +20,13 @@ if (mode == 'dark') {
     modeBtn.checked = true;
     document.documentElement.classList.remove("light")
     document.documentElement.classList.add("dark")
-    document.getElementById("bannerimage").src = ".../images/Welcome-to-the-Silver-Glass-Mobile-Bar(night mode).webp";
+    document.getElementById("bannerimage").src = "../images/Welcome-to-the-Silver-Glass-Mobile-Bar(night mode).webp";
 }
 
 if (mode == 'light') {
     modeBtn.checked = false;
     document.documentElement.classList.remove("dark")
     document.documentElement.classList.add("light")
-    document.getElementById("bannerimage").src = ".../images/Welcome-to-the-Silver-Glass-Mobile-Bar.webp";
+    document.getElementById("bannerimage").src = "../images/Welcome-to-the-Silver-Glass-Mobile-Bar.webp";
 
 }
