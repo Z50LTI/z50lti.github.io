1c29ea087505f0ca391c877d60fe961d25fd83c8
Z50LTI <92446963+Z50LTI@users.noreply.github.com>
2024-05-01 10:53:37 +0200

    Add files via upload

--- js/menu.js
+++ js/menu.js
@@ -1,11 +1,14 @@
 var menu = document.querySelector("nav");
 
 document.getElementById("hamburger").addEventListener("click", () => {
+  menu.style.opacity = "1";
   menu.style.transform = "translateY(0%)";
+
   document.body.style.overflow = "hidden";
 });
 
 document.getElementById("close").addEventListener("click", () => {
   document.body.style.overflow = "auto";
+  menu.style.opacity = "0";
   menu.style.transform = "translateY(100%)";
 });
